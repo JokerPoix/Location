f5ea7e515c7905edda63e41b15a88b83b91580d4
MathieuCoding <polletmathieu.pro@gmail.com>
2023-12-26 16:20:20 +0100

    TP3 WIP

--- lib/views/habitation_details.dart
+++ lib/views/habitation_details.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:location/services/habitation_service.dart';
+import 'package:location/share/location_style.dart';
+import 'package:location/views/share/habitation_option.dart';
 
 class HabitationDetails extends StatefulWidget {
-  const HabitationDetails({super.key});
+  final Habitation _habitation;
+
+  const HabitationDetails(this._habitation, {Key? key}) : super(key: key);
 
   @override
   State<HabitationDetails> createState() => _HabitationDetailsState();
@@ -10,6 +16,70 @@ class HabitationDetails extends StatefulWidget {
 class _HabitationDetailsState extends State<HabitationDetails> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget._habitation.libelle),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(4.0),
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image.asset(
+              'assets/images/locations/${widget._habitation.image}',
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(8.0),
+            child: Text(widget._habitation.adresse),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              HabitationOption(
+                  Icons.group, "${widget._habitation.nbpersonnes} personnes"),
+              HabitationOption(
+                  Icons.bed, "${widget._habitation.chambres} chambres"),
+              HabitationOption(
+                  Icons.fit_screen, "${widget._habitation.superficie} m²"),
+            ],
+          ),
+          _buildRentButton(),
+        ],
+      ),
+    );
+  }
+
+  _buildRentButton() {
+    var format = NumberFormat("### €");
+
+    return Container(
+      decoration: BoxDecoration(
+        color: LocationStyle.backgroundColorPurple,
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      margin: EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              format.format(widget._habitation.prixmois),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                print('Louer habitaiton');
+              },
+              child: Text('Louer'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
